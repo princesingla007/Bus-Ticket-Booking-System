0b8e2f3b96eee3eebd2627a2ed06671bebb718b0
princesingla007 <princesingla9090@gmail.com>
2025-09-26 15:54:39 +0530

    changes

--- bustrainticket/settings.py
+++ bustrainticket/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+import os
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +26,11 @@ SECRET_KEY = 'django-insecure-^#xlxqlhs7_i42t5*n*(&+b$y874ojr#f8-@er-wfx59+j*qy@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "bus-ticket-booking-system-j3v5.onrender.com",  
+    "localhost", 
+    "127.0.0.1",
+    ]
 
 
 # Application definition
